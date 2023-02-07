78251d61a80c61c5fe5cf0b7672dd8c6d1373bd8
bryce-88 <yengpeng.khor@gmail.com>
2023-02-07 16:38:36 +0800

    day12workshop completed

--- src/main/java/ibf2022/ssf/day12workshop/controller/CountryController.java
+++ src/main/java/ibf2022/ssf/day12workshop/controller/CountryController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import ibf2022.ssf.day12workshop.model.Country;
@@ -34,7 +35,8 @@ public class CountryController {
         return ctySvc.createCountry(cty);
     }
 
-    @GetMapping("/list")
+    //@RequestMapping(path = "/list", method = RequestMethod.GET) --> another method to obtain the same result as GetMapping below
+    @GetMapping("list")
     public String displayCountryList(Model model) {
         List<Country> ctyList = ctySvc.getAllCountries();
         model.addAttribute("countryList", ctyList);
---
+++ src/main/java/ibf2022/ssf/day12workshop/controller/WorkshopController.java
@@ -0,0 +1,45 @@
+package ibf2022.ssf.day12workshop.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping(path="workshop")
+public class WorkshopController {
+    
+    @GetMapping()
+    public String workshop() {
+
+        return "workshop";
+    }
+
+    @PostMapping("/processNumber")
+    public String processNumber(@RequestParam(name="inputNumber", defaultValue="1") Integer unit, Model model) {
+
+        List<Integer> lstIntegers = new ArrayList<Integer>();
+
+        Integer loopValue = unit;
+        Integer currentValue = 1;
+
+        while (currentValue <= loopValue) {
+            int result = (int) (Math.random() * loopValue) + 1;
+
+            if (!lstIntegers.contains(Integer.valueOf(result))) {
+                lstIntegers.add(Integer.valueOf(result));
+                currentValue++;
+            }
+        }
+
+        model.addAttribute("numbers", lstIntegers);
+
+        return "workshopresult";
+    }
+}
